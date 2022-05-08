d4cdc5e60f907046dd5cc10a50f44d1559fc27a8
Ahmed AlSardi <A7Dpy@outlook.com>
2022-05-08 18:32:11 +0300

    adding diabete form

--- website/frontend/src/app/app.module.ts
+++ website/frontend/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DiabetesComponent } from './diabetes/diabetes.component';
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSelectModule } from "@angular/material/select";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSliderModule } from "@angular/material/slider";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiabetesComponent
+    DiabetesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- website/frontend/src/app/diabetes/diabetes.component.ts
+++ website/frontend/src/app/diabetes/diabetes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-diabetes',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiabetesComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      preg: [0, [Validators.required, Validators.min(0), Validators.max(20)]],
+      gluc: [40, [Validators.required, Validators.min(30), Validators.max(210)]],
+      bloodpressure: [20, [Validators.required, Validators.min(10), Validators.max(140)]],
+      bmi: [20, [Validators.required, Validators.min(15), Validators.max(90)]],
+      diabete_pedigree_function: [0.01, [Validators.required, Validators.min(0.0001), Validators.max(4)]],
+      age: [25, [Validators.required, Validators.min(18), Validators.max(90)]],
+    });
   }
 
+  get preg(): AbstractControl {
+    return this.form?.get('preg')!;
+  }
+
+  get gluc(): AbstractControl {
+    return this.form?.get('gluc')!;
+  }
+
+  get bloodpressure(): AbstractControl {
+    return this.form?.get('bloodpressure')!;
+  }
+
+  get bmi(): AbstractControl {
+    return this.form?.get('bmi')!;
+  }
+
+  get diabete_pedigree_function(): AbstractControl {
+    return this.form?.get('diabete_pedigree_function')!;
+  }
+
+  get age(): AbstractControl {
+    return this.form?.get('age')!;
+  }
+
+  onSubmit(): void {
+    console.log("ready to submit");
+  }
 }
